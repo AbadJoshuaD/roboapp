7ff4863e0b31a77872ce36ffcb5edb36bd5cba51
Abad Joshua <abad.joshuad@gmail.com>
2019-10-12 01:31:34 +0800

    Updates

---
+++ precache-manifest.f22d649980766d08fa55e82ca33fbed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbb43365f54a4b87e3cfdb879d4e464",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "685916c80cf6353f5f16",
+    "url": "/roboapp/static/css/main.4755eb34.chunk.css"
+  },
+  {
+    "revision": "959027acf0037fa1ce3d",
+    "url": "/roboapp/static/js/2.95f089a3.chunk.js"
+  },
+  {
+    "revision": "685916c80cf6353f5f16",
+    "url": "/roboapp/static/js/main.c666e7de.chunk.js"
+  },
+  {
+    "revision": "02eb9c9b93324bd11970",
+    "url": "/roboapp/static/js/runtime~main.4cc9048e.js"
+  }
+]);
